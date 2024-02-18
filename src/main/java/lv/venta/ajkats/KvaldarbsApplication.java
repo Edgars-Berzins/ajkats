f296f435139579dc140d78275bce4bd3b7396952
Edgars Bērziņš <berzins.edgars.1992@gmail.com>
2024-02-18 12:10:11 +0200

    Initial commit

---
+++ src/main/java/lv/venta/ajkats/KvaldarbsApplication.java
@@ -0,0 +1,13 @@
+package lv.venta.ajkats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KvaldarbsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KvaldarbsApplication.class, args);
+    }
+
+}
